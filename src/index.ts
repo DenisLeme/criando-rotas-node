d2da6239b2f8ba96f6ea5edc6ad286fa4a8f90fc
Denis <denis_leme@icloud.com>
2023-04-11 12:07:00 -0300

    Isolando as rotas

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,16 @@
 import express, {Request, Response} from 'express';
-import { UserController } from './controllers/UserController';
+import { router } from './routes';
 
-
-
-const userController = new UserController()
 
 
 const server = express();
-
 server.use(express.json())
+server.use(router)
 
 server.get('/' , (request: Request, response: Response) => {
     return response.status(200).json({message: 'Criando rotas'})
 
 })
-server.post('/user', userController.createUser)
-server.get('/user', userController.getAllUsers)
+
 
 server.listen(5000, () => console.log('Server on'))
